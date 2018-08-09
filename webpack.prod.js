a4ad9e53f01f328e7c12d97251381e08d6058ed5
cav2008 <cavan.law@bt.com>
2018-08-09 17:07:08 +0100

    update webpack

---
+++ webpack.prod.js
@@ -0,0 +1,58 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: path.resolve(__dirname, 'src/main.js'),
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      { test: /\.js$/, loader: 'babel-loader', exclude: [/node_modules/] },
+      {
+        test: /\.scss$/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader' },
+          { loader: 'resolve-url-loader' },
+          { loader: 'sass-loader' }
+        ]
+      },
+      {
+        test: /\.(ttf|woff|woff2)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 10000,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpg|gif|mp3)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {},
+          },
+        ],
+      },
+    ]
+  },
+  resolve: {
+    modules: ['node_modules'],
+    // Resolve import extension to .js if it is missing.
+    extensions: ['.js'],
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({ template: path.resolve(__dirname, './src/app/index.html'), }),
+    new CopyWebpackPlugin([{ from: path.resolve(__dirname, 'src/assets'), to: path.resolve(__dirname, 'dist/assets')}])
+  ]
+};
